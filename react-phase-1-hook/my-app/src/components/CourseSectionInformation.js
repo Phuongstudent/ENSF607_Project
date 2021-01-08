bd5169b82722a514ea321d68a345166d35c601bf
phuongstudent <pddang@ucalgary.ca>
2021-01-08 15:15:51 -0700

    Don convert to functional react and changed to bulma style

--- react-phase-1-hook/my-app/src/components/CourseSectionInformation.js
+++ react-phase-1-hook/my-app/src/components/CourseSectionInformation.js
@@ -1,86 +1,86 @@
-import React, {useState} from "react";
+// import React, {useState} from "react";
 
 
-function courseSectionRow(row){
+// function courseSectionRow(row){
 
-    return(
-        <tr>
-            <td>
-                {row.category}
-            </td>
-            <td>
-            <input  name = "numSections"
-                    type = "text"
-                    value = {row.numSections}
-                    onChange = {e=>handleChangeRow(e.target.value,row.category)}
-                    />
-            </td>
-            <td>
-            <input  name = "numSections"
-                    type = "text"
-                    value = {row.numSections}
-                    onChange = {e=>handleChangeRow(e.target.value,row.category)}
-                    />
-            </td>
-            <td>
-            <input  name = "numSections"
-                    type = "text"
-                    value = {row.numSections}
-                    onChange = {e=>handleChangeRow(e.target.value,row.category)}
-                    />
-            </td>
+//     return(
+//         <tr>
+//             <td>
+//                 {row.category}
+//             </td>
+//             <td>
+//             <input  name = "numSections"
+//                     type = "text"
+//                     value = {row.numSections}
+//                     onChange = {e=>handleChangeRow(e.target.value,row.category)}
+//                     />
+//             </td>
+//             <td>
+//             <input  name = "numSections"
+//                     type = "text"
+//                     value = {row.numSections}
+//                     onChange = {e=>handleChangeRow(e.target.value,row.category)}
+//                     />
+//             </td>
+//             <td>
+//             <input  name = "numSections"
+//                     type = "text"
+//                     value = {row.numSections}
+//                     onChange = {e=>handleChangeRow(e.target.value,row.category)}
+//                     />
+//             </td>
             
-        </tr>
-    );
+//         </tr>
+//     );
 
-}
+// }
 
-function CourseSectionInformation(){
-    const [data, setData] = React.useState([
-        {category: "Lecture", numSections : "", numHours: "", numStudents: ""},
-        {category: "Tutorial", numSections : "", numHours: "", numStudents: ""},
-        {category: "Lab", numSections : "", numHours: "", numStudents: ""},
-    ]);
+// function CourseSectionInformation(){
+//     const [data, setData] = React.useState([
+//         {category: "Lecture", numSections : "", numHours: "", numStudents: ""},
+//         {category: "Tutorial", numSections : "", numHours: "", numStudents: ""},
+//         {category: "Lab", numSections : "", numHours: "", numStudents: ""},
+//     ]);
 
-    const handleChangeRow = (value,category) =>{
-        const newData = [...data];
-        newData[category].outcome = value; 
-        setData(newData);
-        console.log("Change row"+{value} );
-    };
+//     const handleChangeRow = (value,category) =>{
+//         const newData = [...data];
+//         newData[category].outcome = value; 
+//         setData(newData);
+//         console.log("Change row"+{value} );
+//     };
 
 
-    return(
-       <div>
-           <table name = "CourseContentCategoryTable">
-                <thead>
-                    <tr>
-                        <td>
+//     return(
+//        <div>
+//            <table name = "CourseContentCategoryTable">
+//                 <thead>
+//                     <tr>
+//                         <td>
                             
-                        </td>
-                        <td>
-                            Number of Sections
-                        </td>
-                        <td>
-                            Hours Per Week
-                        </td>
-                        <td>
-                            Number of Students Per Supervisor
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
+//                         </td>
+//                         <td>
+//                             Number of Sections
+//                         </td>
+//                         <td>
+//                             Hours Per Week
+//                         </td>
+//                         <td>
+//                             Number of Students Per Supervisor
+//                         </td>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
 
-                {data.map((row)=>(
-                    <courseSectionRow
-                    row={row}/>
-                ))}
+//                 {data.map((row)=>(
+//                     <courseSectionRow
+//                     row={row}/>
+//                 ))}
 
-                </tbody>
-           </table>
-       </div>
-    );
-}
+//                 </tbody>
+//            </table>
+//        </div>
+//     );
+// }
 
 
-export default CourseSectionInformation;
+// export default CourseSectionInformation;
